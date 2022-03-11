e7e6d2f22a7b8b187d52e97a26ba8593bee3c63c
kadalihiranmayee <hiranmayeekadali22@gmail.com>
2022-03-11 23:25:32 +0530

    Initial commit(hiranmayee)

---
+++ src/main/java/Calculator_functions.java
@@ -0,0 +1,5 @@
+public class Calculator_functions {
+    public int add(int a,int b){
+        return(a+b);
+    }
+}
---
+++ src/test/java/Calculator_functionsTest.java
@@ -0,0 +1,17 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Calculator_functionsTest {
+    public static Calculator_functions obj;
+
+    @BeforeClass
+    public static void setup(){obj = new Calculator_functions();}
+
+    @Test
+    public void test_add() {
+        assertEquals(4,obj.add(2,2));
+
+    }
+}
