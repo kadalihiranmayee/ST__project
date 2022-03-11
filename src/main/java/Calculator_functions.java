9c3b591c70091a1b4e6cadb0539a3617e6151f6d
Murtaza <munv21@student.bth.se>
2022-03-11 23:04:57 +0100

    Commit Done by Murtaza Naqvi corrected main functionality

--- src/main/java/Calculator_functions.java
+++ src/main/java/Calculator_functions.java
@@ -3,7 +3,7 @@ public class Calculator_functions {
         return(a+b);
     }
     public int sub(int c, int d){
-//        return(c-d);
-        return 0;
+        return(c-d);
+//        return 0;
     }
    }
