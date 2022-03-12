8c6b48f5395c9872ca13108b023b1ac754bd145c
kadalihiranmayee <hiranmayeekadali22@gmail.com>
2022-03-13 04:41:29 +0530

    Irene's commit to git(right functionality and right test case)

--- src/main/java/Calculator_functions.java
+++ src/main/java/Calculator_functions.java
@@ -9,7 +9,7 @@ public class Calculator_functions{
         return (a*b);
     }
     public int div(int a,int b){
-       // return (a/b);
-        return 0;
+       return (a/b);
+       // return 0;
     }
 }
