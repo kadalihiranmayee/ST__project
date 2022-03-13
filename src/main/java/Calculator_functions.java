ce059186f58fba0d49d96ca6a6bacb8c365899fa
kadalihiranmayee <hiranmayeekadali22@gmail.com>
2022-03-13 23:51:50 +0530

    Integrated testing

--- src/main/java/Calculator_functions.java
+++ src/main/java/Calculator_functions.java
@@ -1,6 +1,11 @@
 public class Calculator_functions {
     public int add(int a, int b) {
         return (a + b);
-        //return 0;
+    }
+    public int sub(int a, int b) {
+        return (a - b);
+    }
+    public int mul(int a, int b) {
+        return (a * b);
     }
 }
--- src/test/java/Calculator_functionsTest.java
+++ src/test/java/Calculator_functionsTest.java
@@ -12,4 +12,12 @@ public class Calculator_functionsTest {
     public void test_add(){
         assertEquals(10,obj.add(5,5));
     }
+    @Test
+    public void integration_test1(){
+        assertEquals(4,obj.mul(obj.add(1,1),obj.sub(4,2)));
+    }
+    @Test
+    public void integration_test2(){
+        assertEquals(3,obj.sub(obj.add(2,2),obj.mul(1,1)));
+    }
 }
